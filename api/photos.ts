99af8f2ec9a2900f929b401743044a4a25ef8e69
ThomasHedan <thomas.hedan56@gmail.com>
2026-05-02 13:24:34 +0200

    feat: update max loading images

--- api/photos.ts
+++ api/photos.ts
@@ -23,7 +23,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   const result = await getS3().send(new ListObjectsV2Command({
     Bucket:            BUCKET_NAME,
-    MaxKeys:           500,
+    MaxKeys:           75,
     ContinuationToken: cursor,
   }));
 
